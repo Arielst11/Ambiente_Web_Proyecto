fdc099d25629de1f9717cdd99ed3a45f7b037348
Arielst11 <asancheztroz@gmail.com>
2023-03-27 18:24:25 -0600

    borrado de imagen del card
    borrado de imagen, no funciona por el momento

--- Proyecto Ambiente Web/PuntosEntrega.php
+++ Proyecto Ambiente Web/PuntosEntrega.php
@@ -16,7 +16,6 @@ include_once 'Include\navbar.html';
 
 <div class="card"  >
 <h5 class="card-title">Correos de Costa Rica Heredia</h5>
-  <img height="226px" width="225px" src="https://lh5.googleusercontent.com/p/AF1QipNE4PDRZksjok--0pkD61Ok6jkBUssZD8bXbBuJ=w408-h454-k-no" class="card-img-top" alt="...">
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31435.918950899206!2d-84.17558088437497!3d9.976332500000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0fb4389360f7d%3A0xce6ada6080813eae!2sCorreos%20de%20Costa%20Rica!5e0!3m2!1ses!2scr!4v1679353879626!5m2!1ses!2scr" height="225px" width="225px style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   <div class="card-body">
     <p class="card-text">De la plaza de Barreal 175 metros oeste, Multicomercial Baden local n° 8 Heredia, Belén, 40104, tel: 21026029</p>
@@ -26,7 +25,7 @@ include_once 'Include\navbar.html';
 
 <div class="card"  >
 <h5 class="card-title">Correos de Costa Rica Heredia</h5>
-  <img height="226px" width="225px" src="https://lh5.googleusercontent.com/p/AF1QipNE4PDRZksjok--0pkD61Ok6jkBUssZD8bXbBuJ=w408-h454-k-no" class="card-img-top" alt="...">
+  
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31435.918950899206!2d-84.17558088437497!3d9.976332500000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0fb4389360f7d%3A0xce6ada6080813eae!2sCorreos%20de%20Costa%20Rica!5e0!3m2!1ses!2scr!4v1679353879626!5m2!1ses!2scr" height="225px" width="225px style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   <div class="card-body">
     <p class="card-text">De la plaza de Barreal 175 metros oeste, Multicomercial Baden local n° 8 Heredia, Belén, 40104, tel: 21026029</p>
@@ -36,7 +35,7 @@ include_once 'Include\navbar.html';
 </div>
 <div class="card"  >
 <h5 class="card-title">Correos de Costa Rica Heredia</h5>
-  <img height="226px" width="225px" src="https://lh5.googleusercontent.com/p/AF1QipNE4PDRZksjok--0pkD61Ok6jkBUssZD8bXbBuJ=w408-h454-k-no" class="card-img-top" alt="...">
+
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31435.918950899206!2d-84.17558088437497!3d9.976332500000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0fb4389360f7d%3A0xce6ada6080813eae!2sCorreos%20de%20Costa%20Rica!5e0!3m2!1ses!2scr!4v1679353879626!5m2!1ses!2scr" height="225px" width="225px style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   <div class="card-body">
     <p class="card-text">De la plaza de Barreal 175 metros oeste, Multicomercial Baden local n° 8 Heredia, Belén, 40104, tel: 21026029</p>
@@ -46,7 +45,7 @@ include_once 'Include\navbar.html';
   </div>
 <div class="card"  >
 <h5 class="card-title">Correos de Costa Rica Heredia</h5>
-  <img height="226px" width="225px" src="https://lh5.googleusercontent.com/p/AF1QipNE4PDRZksjok--0pkD61Ok6jkBUssZD8bXbBuJ=w408-h454-k-no" class="card-img-top" alt="...">
+
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31435.918950899206!2d-84.17558088437497!3d9.976332500000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0fb4389360f7d%3A0xce6ada6080813eae!2sCorreos%20de%20Costa%20Rica!5e0!3m2!1ses!2scr!4v1679353879626!5m2!1ses!2scr" height="225px" width="225px style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   <div class="card-body">
     <p class="card-text">De la plaza de Barreal 175 metros oeste, Multicomercial Baden local n° 8 Heredia, Belén, 40104, tel: 21026029</p>
