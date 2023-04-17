52d9e8ae92eb30811eea3d729093188f66ce02c7
waritojr <102387525+waritojr@users.noreply.github.com>
2023-04-16 19:38:02 -0600

    Update MenuArticulos.php

--- Proyecto Ambiente Web/MenuArticulos.php
+++ Proyecto Ambiente Web/MenuArticulos.php
@@ -11,31 +11,44 @@ include_once 'Include\head.html';
 include_once 'Include\navbar.php';
 ?>
 
+<main>
 
 <h1>Manejo de Artículos </h1>
 <hr>
 
 
-
 <div class="gridcontainer-submenu-articulos">
+  <div class="submenu-articulo">
+    <h2>Insertar</h2> 
+    <a href="InsertarArticulo.php">
+      <img src="Images/order.png" alt="Insertar Artículo">
+    </a> 
+  </div>
 
-<div class="submenu-articulo">
-<h2> Insertar </h2> 
-<a href="InsertarArticulo.php"><img height="300px" src="Images/order.png"></a> 
+  <div class="submenu-articulo">
+    <h2>Modificar</h2> 
+    <a href="ModificarArticulo.php">
+      <img src="Images/edit.png" alt="Modificar Artículo">
+    </a> 
+  </div>
+
+  <div class="submenu-articulo">
+    <h2>Eliminar</h2> 
+    <a href="EliminarArticulo.php">
+      <img src="Images/trash.png" alt="Eliminar Artículo">
+    </a> 
+  </div>
 </div>
 
-<div class="submenu-articulo">
-<h2> Modificar </h2> 
-<a href="ModificarArticulo.php"><img height="300px" src="Images/edit.png"></a> 
-</div>
+</main>
 
-<div class="submenu-articulo">
-<h2> Eliminar </h2> 
-<a href="EliminarArticulo.php"><img height="300px" src="Images/trash.png"></a> 
-</div>
+<footer>
+<?php
+include_once 'Include\footer.html';
 
+?>
+</footer>
 
-</div>
 </body>
 
 
