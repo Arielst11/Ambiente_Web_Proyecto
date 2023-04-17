e3567de44c5af6051492dc9bbdfbd93097fa1ad4
waritojr <102387525+waritojr@users.noreply.github.com>
2023-04-16 19:27:46 -0600

    Add files via upload

---
+++ Proyecto Ambiente Web/Include/footer.php
@@ -0,0 +1,46 @@
+<?php
+
+echo    "<footer class='pie-pagina'>";
+
+echo    "<div class='grupo-1'>";
+
+echo    "<div class='box'>";
+
+echo    "<h2>SOBRE NOSOTROS</h2>";
+
+echo    "<p>Empresa de Costa Rica dedidaca a la importacion y venta de articulos</p>";
+
+echo    "</div>";
+
+echo    "<div class='box'>";
+
+echo    "<h2>SIGUENOS</h2>";
+
+echo    "<div class='red-social'>";
+
+echo    "<a href='#'>";
+
+echo    "<img src='Images/facebook.png' alt='Facebook' width='60px' height='60px'>";
+
+echo    "</a>";   
+
+echo    "<a href='#'>";
+
+echo    "<img src=Images/instagram.png alt='Instagram' width='60px' height='60px'>";
+
+echo    "</a>";
+
+echo    "</div>";
+
+echo    "</div>";
+            
+echo    "</div>";
+
+echo    "<div class='grupo-2'>";
+
+echo    "<small>&copy; 2023 <b>Importaciones Costa Rica</b> - Todos los derechos Reservados. </small>";
+
+echo    "</div>";
+
+echo    "</footer>";
+?>
